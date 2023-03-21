158e97bb3262d053908d454b87e4b5a769607c27
Michael McCarthy <mike@pixelonezero.com>
2023-03-20 20:17:30 -0700

    Set current weather and daily weather in separate state variables. Output current weather component.

--- src/components/CurrentWeatherLoader.js
+++ src/components/CurrentWeatherLoader.js
@@ -3,21 +3,40 @@ import axios from 'axios';
 
 export const CurrentWeatherLoader = () => {
     
-    const [weather, setWeather] = useState({});
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [dailyWeather, setDailyWeather] = useState(null);
 
     useEffect(() => {
         (async () => {
-            const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=37.7560855&lon=-122.2308277&appid=2527277900597fb91adae3d61344b49e');
-            setWeather(response.data);
-            console.log(response.data);
+            const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=37.7560855&lon=-122.2308277&appid=2527277900597fb91adae3d61344b49e&units=imperial');
+            setCurrentWeather(response.data.current);
+            setDailyWeather(response.data.daily);
         })();
     }, [])
 
     return (
         <>
-            <code>
-                {JSON.stringify(weather)}
-            </code>
+            {currentWeather && 
+                <div style={{width:'50%', float:'left', display:'inline-block'}}>
+                    <h3>
+                        Current Weather
+                    </h3>
+                    <ul>
+                        <li>
+                            {currentWeather.temp} &deg;F
+                        </li>
+                        <li>
+                            {currentWeather.weather && currentWeather.weather[0].description}
+                        </li>
+                        <li>
+                            {currentWeather.humidity}% humidity
+                        </li>
+                        <li>
+                            wind {currentWeather.wind_speed} m.p.h.
+                        </li>
+                    </ul>
+                </div>
+            }
         </>
     );
 }
